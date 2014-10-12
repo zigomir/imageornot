27b67bddb6573bf85bff1524e2505c5f7478eedc
zigomir <zigomir@gmail.com>
2014-10-12 23:51:44 +0200

    wow

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'JSON'
+require 'faraday'
+
+class ImageOrNot < Sinatra::Application
+
+  options '/' do
+    cross_origin
+  end
+
+  post '/' do
+    cross_origin
+
+    request.body.rewind  # in case someone already read it
+    data = JSON.parse request.body.read
+    json is_image: false unless data['url']
+
+    begin
+      response = Faraday.head data['url']
+      content_type = response.headers[:'content-type']
+
+      json is_image: is_image?(content_type)
+    rescue Exception
+      json is_image: false
+    end
+  end
+
+private
+
+  def is_image?(content_type)
+    content_type and
+    content_type.split('/').length == 2 and
+    content_type.split('/').first == 'image'
+  end
+
+end
---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'sinatra/cross_origin'
+
+require './app'
+run ImageOrNot
